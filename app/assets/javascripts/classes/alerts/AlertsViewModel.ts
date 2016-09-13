2ed4a23d168a406daf9fd2407c3141050201be21
Brandon Arp <brandon.arp@smartsheet.com>
2016-09-13 15:39:00 -0700

    refactor paginated lists

--- app/assets/javascripts/classes/alerts/AlertsViewModel.ts
+++ app/assets/javascripts/classes/alerts/AlertsViewModel.ts
@@ -14,88 +14,20 @@
  * limitations under the License.
  */
 
-///<reference path="../../libs/knockout/knockout.d.ts" />
 ///<reference path="../../libs/jqueryui/jqueryui.d.ts"/>
-import ko = require('knockout');
 import AlertData = require('./AlertData');
+import PaginatedSearchableList = require('../PaginatedSearchableList');
 import $ = require('jquery');
 
-interface PagerElement { name: string; page: number; disabled: boolean; active: boolean}
-
-class AlertsViewModel {
-    filteredAlerts: KnockoutObservableArray<AlertData> = ko.observableArray<AlertData>();
-    searchAlert: KnockoutObservable<string> = ko.observable('');
-    searchAlertThrottled: KnockoutComputed<string> = ko.computed<string>(() => {
-        return this.searchAlert();
-    }).extend({rateLimit: {timeout: 400, method: "notifyWhenChangesStop"}});
-    page: KnockoutObservable<number> = ko.observable(1);
-    pagesMax: KnockoutObservable<number> = ko.observable(1);
-    private pagerElements: KnockoutComputed<PagerElement[]>;
-    private perPage = 12;
-
+class AlertsViewModel extends PaginatedSearchableList<AlertData> {
     constructor() {
-        var self = this;
-        this.searchAlertThrottled.subscribe(() => {this.page(1); this.query();});
+        super();
         this.query();
-
-        this.pagerElements = ko.computed(() => {
-            var p = this.page();
-            var elements: PagerElement[] = [];
-
-            var prevDisabled = p == 1;
-            elements.push({name: "Prev", page: p - 1, disabled: prevDisabled, active: false});
-            if (p > 3) {
-                elements.push({name: "1", page: 1, disabled: false, active: false});
-            }
-
-            if (p > 4) {
-                elements.push({name: "...", page: 0, disabled: true, active: false});
-            }
-
-            for (var i = p - 2; i <= p + 2; i++) {
-                if (i >= 1 && i <= this.pagesMax()) {
-                    var active = i == p;
-                    elements.push({name: String(i), page: i, disabled: false, active: active});
-                }
-            }
-
-            if (p <= this.pagesMax() - 4) {
-                elements.push({name: "...", page: 0, disabled: true, active: false});
-            }
-
-            if (p <= this.pagesMax() - 3) {
-                var last = this.pagesMax();
-                elements.push({name: String(last), page: last, disabled: false, active: false});
-            }
-
-            var nextDisabled = p == this.pagesMax();
-            elements.push({name: "Next", page: p + 1, disabled: nextDisabled, active: false});
-
-            return elements;
-        }, self);
-
-        this.gotoPage = (element: PagerElement) => {
-            if (element.disabled || element.page == this.page()) {
-                return;
-            }
-            this.page(element.page);
-            this.query();
-        }
     }
 
-    private gotoPage: (element: PagerElement) => void;
-
-    query() {
-        var alert = this.searchAlertThrottled();
-        var offset = (this.page() - 1) * this.perPage;
-        var query: any = {limit: this.perPage, offset: offset};
-        if (alert && alert != "") {
-            query.contains = alert;
-        }
+    fetchData(query, callback) {
         $.getJSON("/v1/alerts/query", query, (data) => {
-            var alertsList: AlertData[] = data.data;
-            this.filteredAlerts.removeAll();
-            this.filteredAlerts(alertsList.map((v: AlertData)=> { return new AlertData(
+            var alertsList: AlertData[] = data.data.map((v: AlertData)=> { return new AlertData(
                 v.id,
                 v.context,
                 v.name,
@@ -107,18 +39,8 @@ class AlertsViewModel {
                 v.operator,
                 v.value,
                 v.extensions
-            );}));
-            var pages = Math.floor(data.pagination.total / this.perPage) + 1;
-            // If total is a multiple of the page size
-            if (data.pagination.total % this.perPage == 0) {
-                pages--;
-            }
-            this.pagesMax(pages);
-            this.page(Math.floor(data.pagination.offset / this.perPage) + 1);
-            if (this.filteredAlerts().length == 0 && this.page() > 1) {
-                this.page(this.page() - 1);
-                this.query();
-            }
+            );});
+            callback(alertsList, data.pagination);
         });
     }
 }
--- app/assets/javascripts/classes/hostregistry/HostRegistryViewModel.ts
+++ app/assets/javascripts/classes/hostregistry/HostRegistryViewModel.ts
@@ -16,116 +16,45 @@
 
 ///<reference path="../../libs/knockout/knockout.d.ts" />
 ///<reference path="../../libs/jqueryui/jqueryui.d.ts"/>
-import ko = require('knockout');
 import HostData = require('./HostData');
+import PaginatedSearchableList = require('../PaginatedSearchableList');
 import Hosts = require('../Hosts');
 import MetricsSoftwareState = require('./MetricsSoftwareState');
 import GraphViewModel = require('../GraphViewModel');
 import $ = require('jquery');
+import ko = require('knockout');
 
-interface PagerElement { name: string; page: number; disabled: boolean; active: boolean}
-
-class HostRegistryViewModel {
-    filteredHosts: KnockoutObservableArray<HostData> = ko.observableArray<HostData>();
-    searchHost: KnockoutObservable<string> = ko.observable('');
-    searchHostThrottled: KnockoutComputed<string> = ko.computed<string>(() => {
-        return this.searchHost();
-    }).extend({rateLimit: {timeout: 400, method: "notifyWhenChangesStop"}});
-    versionFilter: KnockoutObservable<string> = ko.observable('');
-    page: KnockoutObservable<number> = ko.observable(1);
-    pagesMax: KnockoutObservable<number> = ko.observable(1);
-    private pagerElements: KnockoutComputed<PagerElement[]>;
-    private perPage = 80;
-
+class HostRegistryViewModel extends PaginatedSearchableList<HostData> {
+    versionFilter: KnockoutObservable <string> = ko.observable('');
 
     constructor() {
-        var self = this;
-        this.searchHostThrottled.subscribe(() => {this.page(1); this.query();});
+        super();
         this.versionFilter.subscribe(() => {this.page(1); this.query()});
         this.query();
-
-        this.pagerElements = ko.computed(() => {
-            var p = this.page();
-            var elements: PagerElement[] = [];
-
-            var prevDisabled = p == 1;
-            elements.push({name: "Prev", page: p - 1, disabled: prevDisabled, active: false});
-            if (p > 3) {
-                elements.push({name: "1", page: 1, disabled: false, active: false});
-            }
-
-            if (p > 4) {
-                elements.push({name: "...", page: 0, disabled: true, active: false});
-            }
-
-            for (var i = p - 2; i <= p + 2; i++) {
-                if (i >= 1 && i <= this.pagesMax()) {
-                    var active = i == p;
-                    elements.push({name: String(i), page: i, disabled: false, active: active});
-                }
-
-            }
-
-            if (p <= this.pagesMax() - 4) {
-                elements.push({name: "...", page: 0, disabled: true, active: false});
-            }
-
-            if (p <= this.pagesMax() - 3) {
-                var last = this.pagesMax();
-                elements.push({name: String(last), page: last, disabled: false, active: false});
-            }
-
-            var nextDisabled = p == this.pagesMax();
-            elements.push({name: "Next", page: p + 1, disabled: nextDisabled, active: false});
-
-            return elements;
-        }, self);
-
-        this.gotoPage = (element: PagerElement) => {
-            if (element.disabled || element.page == this.page()) {
-                return;
-            }
-            this.page(element.page);
-            this.query();
-        }
-    }
+    };
 
     click(connectTo: HostData) {
         Hosts.connectToServer(connectTo.hostname);
     }
 
-    private gotoPage: (element: PagerElement) => void;
-
-
-    query() {
-        var host = this.searchHostThrottled();
+    amendQuery(query) {
+        var host = this.searchExpressionThrottled();
         var version = this.versionFilter();
-        var offset = (this.page() - 1) * this.perPage;
-        var query: any = {limit: this.perPage, sort_by: "HOSTNAME", offset: offset};
         if (host && host != "") {
             query.name = host;
         }
         if (version && version != "") {
             query.state = version;
         }
-        $.getJSON("/v1/hosts/query", query, (data) => {
-            var hostsList: HostData[] = data.data;
-            this.filteredHosts.removeAll();
-            this.filteredHosts(hostsList.map((v: HostData)=> { return new HostData(v.hostname, v.metricsSoftwareState);}));
-            var pages = Math.floor(data.pagination.total / this.perPage) + 1;
-            // If total is a multiple of the page size
-            if (data.pagination.total % this.perPage == 0) {
-                pages--;
-            }
-            this.pagesMax(pages);
-            this.page(Math.floor(data.pagination.offset / this.perPage) + 1);
-            if (this.filteredHosts().length == 0 && this.page() > 1) {
-                this.page(this.page() - 1);
-                this.query();
-            }
-        });
     }
 
+    fetchData(query, callback) {
+        $.getJSON("/v1/hosts/query", query, (data) => {
+            var hostList: HostData[] = data.data.map((v: HostData)=> {
+                return new HostData(v.hostname, v.metricsSoftwareState);});
+            callback(hostList, data.pagination);
+        });
+    }
 }
 
 export = HostRegistryViewModel;
