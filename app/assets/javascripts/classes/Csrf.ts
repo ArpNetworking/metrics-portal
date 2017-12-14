fd52be8727a119d36534161c2d4c7ebd32865a20
Brandon Arp <brandon.arp@smartsheet.com>
2017-12-13 16:02:38 -0800

    fixes for CSRF with karios and alerts endpoints

---
+++ app/assets/javascripts/classes/Csrf.ts
@@ -0,0 +1,10 @@
+import jwt_decode = require("jwt-decode");
+
+export default class Csrf {
+    public static getToken() {
+        let allCookies = document.cookie;
+        let sessionCookie = allCookies.split("; ").find(value => value.split("=")[0] == "PLAY_SESSION").split("=")[1];
+        let decoded = jwt_decode(sessionCookie);
+        return decoded["data"]["csrfToken"];
+    }
+}
