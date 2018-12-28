52f3bd44277b4f9f8838a35dcd67cdf8a90a6e2e
Ville Koskela <vkoskela@dropbox.com>
2018-12-28 14:27:45 -0800

    Upgrade ebean and play enehancer as a requirement for Maven migration.

--- app/models/ebean/AlertEtags.java
+++ app/models/ebean/AlertEtags.java
@@ -27,6 +27,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.util.Optional;
 
 /**
  * Model class to represent alert etag records.
