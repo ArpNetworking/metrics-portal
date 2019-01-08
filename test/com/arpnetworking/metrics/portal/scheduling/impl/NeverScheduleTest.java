5af841ca6fdbfba5e74d49231cf77648547002d1
Spencer Pearson <spencerpearson@dropbox.com>
2019-01-07 16:55:15 -0800

    remove unused constant

--- test/com/arpnetworking/metrics/portal/scheduling/impl/NeverScheduleTest.java
+++ test/com/arpnetworking/metrics/portal/scheduling/impl/NeverScheduleTest.java
@@ -29,8 +29,6 @@ import static org.junit.Assert.assertEquals;
  */
 public final class NeverScheduleTest {
 
-    private static final ZonedDateTime t0 = ZonedDateTime.parse("2018-01-01T00:00:00Z");
-
     @Test
     public void testNextRun() {
         // Two interesting cases: lastRun=null, lastRun!=null
