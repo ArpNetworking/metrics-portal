3b2e4f63f18148a69d01479bf768d8436ad809b1
Ville Koskela <vkoskela@dropbox.com>
2019-03-28 13:01:13 -0700

    JDBC clustering

---
+++ conf/db/migration/akka/h2/V1__akka_persistence_jdbc.sql
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2019 Dropbox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+/**
+ * Based on:
+ *
+ * https://github.com/dnvriend/akka-persistence-jdbc/blob/master/src/test/resources/schema/h2/h2-schema.sql
+ */
+
+DROP TABLE IF EXISTS akka.journal;
+
+CREATE TABLE IF NOT EXISTS akka.journal (
+  "ordering" BIGINT AUTO_INCREMENT,
+  "persistence_id" VARCHAR(255) NOT NULL,
+  "sequence_number" BIGINT NOT NULL,
+  "deleted" BOOLEAN DEFAULT FALSE,
+  "tags" VARCHAR(255) DEFAULT NULL,
+  "message" BYTEA NOT NULL,
+  PRIMARY KEY("persistence_id", "sequence_number")
+);
+
+CREATE UNIQUE INDEX "journal_ordering_idx" ON akka.journal("ordering");
+
+DROP TABLE IF EXISTS akka.snapshot;
+
+CREATE TABLE IF NOT EXISTS akka.snapshot (
+  "persistence_id" VARCHAR(255) NOT NULL,
+  "sequence_number" BIGINT NOT NULL,
+  "created" BIGINT NOT NULL,
+  "snapshot" BYTEA NOT NULL,
+  PRIMARY KEY("persistence_id", "sequence_number")
+);
