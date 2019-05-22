63df72d66aca06c2f057b98b48bc37a7e4e3b830
Spencer Pearson <spencerpearson@dropbox.com>
2019-05-22 16:37:34 -0700

    compare entire objects in ReportDeserializationTest

--- test/java/models/view/ReportSerializationTest.java
+++ test/java/models/view/ReportSerializationTest.java
@@ -18,6 +18,7 @@ package models.view;
 
 import com.arpnetworking.metrics.portal.reports.RecipientType;
 import com.arpnetworking.utility.ResourceHelper;
+import com.google.common.collect.Lists;
 import models.internal.scheduling.Period;
 import models.view.impl.ChromeScreenshotReportSource;
 import models.view.impl.HtmlReportFormat;
@@ -25,6 +26,7 @@ import models.view.impl.OneOffSchedule;
 import models.view.impl.PeriodicSchedule;
 import models.view.reports.Recipient;
 import models.view.reports.Report;
+import models.view.reports.ReportSource;
 import models.view.scheduling.Schedule;
 import org.junit.Test;
 
@@ -32,7 +34,9 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.ZoneId;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -44,28 +48,57 @@ import static org.junit.Assert.assertTrue;
  */
 public final class ReportSerializationTest {
     @Test
-    public void testValidReport() throws IOException, URISyntaxException {
-        final Report report = loadResourceAs("testValidReport", Report.class);
-        assertEquals(new URI("https://example.com"), ((ChromeScreenshotReportSource) report.getSource()).getUri());
-        assertTrue(report.getSchedule() instanceof OneOffSchedule);
-        assertEquals(1, report.getRecipients().size());
-        assertEquals("nobody@example.com", report.getRecipients().get(0).getAddress());
+    public void testValidReport() throws IOException {
+        final Report expected = new Report();
+        expected.setId(UUID.fromString("11111111-1111-1111-1111-111111111111"));
+        expected.setName("My Report Name");
+
+        final ChromeScreenshotReportSource expectedSource = new ChromeScreenshotReportSource();
+        expectedSource.setId(UUID.fromString("22222222-2222-2222-2222-222222222222"));
+        expectedSource.setUri(URI.create("https://example.com"));
+        expectedSource.setTitle("My Report Title");
+        expectedSource.setIgnoreCertificateErrors(false);
+        expectedSource.setTriggeringEventName("myTriggeringEventName");
+        expected.setSource(expectedSource);
+
+        final OneOffSchedule expectedSchedule = new OneOffSchedule();
+        expectedSchedule.setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"));
+        expected.setSchedule(expectedSchedule);
+
+        final Recipient expectedRecipient = new Recipient();
+        expectedRecipient.setType(RecipientType.EMAIL);
+        expectedRecipient.setId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
+        expectedRecipient.setAddress("nobody@example.com");
+        expectedRecipient.setFormat(new HtmlReportFormat());
+        expected.setRecipients(Lists.newArrayList(expectedRecipient));
+
+        final Report actual = loadResourceAs("testValidReport", Report.class);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testValidPeriodicSchedule() throws IOException {
-        final PeriodicSchedule schedule = (PeriodicSchedule) loadResourceAs("testValidPeriodicSchedule", Schedule.class);
-        assertEquals(Period.DAILY, schedule.getPeriod());
-        assertEquals(Duration.parse("PT4H"), schedule.getOffset());
-        assertEquals(ZoneId.of("America/Los_Angeles"), schedule.getZone());
+        final PeriodicSchedule expected = new PeriodicSchedule();
+        expected.setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"));
+        expected.setRunUntil(Instant.parse("2020-01-01T00:00:00Z"));
+        expected.setPeriod(Period.DAILY);
+        expected.setZone(ZoneId.of("America/Los_Angeles"));
+        expected.setOffset(Duration.parse("PT4H"));
+
+        final Schedule actual = loadResourceAs("testValidPeriodicSchedule", Schedule.class);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testValidRecipient() throws IOException {
-        final Recipient recipient = loadResourceAs("testValidRecipient", Recipient.class);
-        assertEquals("nobody@example.com", recipient.getAddress());
-        assertEquals(RecipientType.EMAIL, recipient.getType());
-        assertEquals(new HtmlReportFormat(), recipient.getFormat());
+        final Recipient expected = new Recipient();
+        expected.setType(RecipientType.EMAIL);
+        expected.setId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
+        expected.setAddress("nobody@example.com");
+        expected.setFormat(new HtmlReportFormat());
+
+        final Recipient actual = loadResourceAs("testValidRecipient", Recipient.class);
+        assertEquals(expected, actual);
     }
 
     @Test(expected = com.fasterxml.jackson.databind.exc.InvalidFormatException.class)
