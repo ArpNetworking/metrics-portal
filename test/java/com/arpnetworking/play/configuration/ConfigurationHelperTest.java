581135f0f2803ea5c790d2f0b27099394e17d9e2
Spencer Pearson <spencerpearson@dropbox.com>
2019-06-04 16:22:54 -0700

    checkstyle

--- test/java/com/arpnetworking/play/configuration/ConfigurationHelperTest.java
+++ test/java/com/arpnetworking/play/configuration/ConfigurationHelperTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Dropbox, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.play.configuration;
 
 import com.google.common.collect.ImmutableMap;
@@ -10,8 +25,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import play.Environment;
 
-import java.util.Objects;
-
+/**
+ * Tests for {@link ConfigurationHelper}.
+ *
+ * @author Spencer Pearson (spencerpearson at dropbox dot com)
+ */
 public class ConfigurationHelperTest {
     @Test
     public void testGetInstance() {
@@ -26,7 +44,7 @@ public class ConfigurationHelperTest {
 
     private static final class InstantiatedObject {
         @Inject
-        public InstantiatedObject(@Assisted final Config config) {
+        InstantiatedObject(@Assisted final Config config) {
             _name = config.getString("name");
         }
 
