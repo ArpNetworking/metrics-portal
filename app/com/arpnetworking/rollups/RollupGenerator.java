ad3aa9b3d5b3d38168dca8764f8b897b35c5c48e
Gilligan Markham <gmarkham@dropbox.com>
2019-06-20 12:05:05 -0700

    Removed unused import.

--- app/com/arpnetworking/rollups/RollupGenerator.java
+++ app/com/arpnetworking/rollups/RollupGenerator.java
@@ -35,7 +35,6 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.typesafe.config.Config;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import scala.concurrent.duration.FiniteDuration;
 
 import java.time.Clock;
