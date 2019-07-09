a4851657d1949d0d34d3b7a9d5a5a8ed69111983
Spencer Pearson <spencerpearson@dropbox.com>
2019-07-09 10:52:15 -0700

    dummy commit to make Travis rerun

--- test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/HtmlScreenshotRendererIT.java
+++ test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/HtmlScreenshotRendererIT.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.arpnetworking.metrics.portal.reports.impl.chrome;
 
 import com.arpnetworking.metrics.portal.TestBeanFactory;
