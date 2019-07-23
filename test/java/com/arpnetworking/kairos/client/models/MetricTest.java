c025b3d9e273c5e530a8d7303e45a2aa535e66c0
Spencer Pearson <spencerpearson@dropbox.com>
2019-07-23 14:38:22 -0700

    lint

--- test/java/com/arpnetworking/kairos/client/models/MetricTest.java
+++ test/java/com/arpnetworking/kairos/client/models/MetricTest.java
@@ -1,11 +1,23 @@
+/*
+ * Copyright 2019 Dropbox, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.kairos.client.models;
 
 import com.arpnetworking.commons.jackson.databind.ObjectMapperFactory;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.collect.Lists;
 import io.ebeaninternal.util.IOUtils;
 import org.junit.Test;
 
@@ -14,6 +26,11 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+/**
+ * Tests for {@link Metric}.
+ *
+ * @author Spencer Pearson (spencerpearson at dropbox dot com)
+ */
 public class MetricTest {
 
     private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.getInstance();
