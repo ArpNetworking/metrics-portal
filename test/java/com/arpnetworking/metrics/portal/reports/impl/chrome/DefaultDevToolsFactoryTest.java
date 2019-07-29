a6f1aac6d24bd4584a4edd2ec703ef6b5e1d2b46
Spencer Pearson <spencerpearson@dropbox.com>
2019-07-29 09:08:12 -0700

    lint

--- test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/DefaultDevToolsFactoryTest.java
+++ test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/DefaultDevToolsFactoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Dropbox, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.metrics.portal.reports.impl.chrome;
 
 import com.google.common.collect.ImmutableList;
@@ -10,11 +25,15 @@ import com.typesafe.config.ConfigValueFactory;
 import org.junit.Test;
 
 import java.time.format.DateTimeParseException;
-
 import javax.annotation.Nullable;
 
 import static org.junit.Assert.fail;
 
+/**
+ * Tests for {@link DefaultDevToolsFactory}.
+ *
+ * @author Spencer Pearson (spencerpearson at dropbox dot com)
+ */
 public class DefaultDevToolsFactoryTest {
 
     private static final Config VALID_CONFIG = ConfigFactory.parseMap(ImmutableMap.of(
@@ -108,13 +127,19 @@ public class DefaultDevToolsFactoryTest {
         }
     }
 
-    private <E extends Exception> void assertInvalidates(final String field, @Nullable final Object badValue, Class<E> clazz) {
+    private <E extends Exception> void assertInvalidates(final String field, @Nullable final Object badValue, final Class<E> clazz) {
         try {
             new DefaultDevToolsFactory(VALID_CONFIG.withValue(field, ConfigValueFactory.fromAnyRef(badValue)));
             fail("setting " + field + "=" + badValue + " should have made constructor throw " + clazz.getName());
+            // CHECKSTYLE.OFF: IllegalCatch
         } catch (final Exception e) {
+            // CHECKSTYLE.ON: IllegalCatch
             if (!clazz.isInstance(e)) {
-                fail("setting " + field + "=" + badValue + " should have made constructor throw " + clazz.getName() + ", not " + e.getClass().getName());
+                fail(
+                        "setting " + field + "=" + badValue
+                                + " should have made constructor throw " + clazz.getName()
+                                + ", not " + e.getClass().getName()
+                );
             }
         }
     }
