3b1f7ec5389fc5cd1ca32949652ac218ec2207ba
Spencer Pearson <spencerpearson@dropbox.com>
2019-09-09 16:36:02 -0700

    lint

--- test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/OriginConfigTest.java
+++ test/java/com/arpnetworking/metrics/portal/reports/impl/chrome/OriginConfigTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Dropbox, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.arpnetworking.metrics.portal.reports.impl.chrome;
 
 import com.arpnetworking.commons.jackson.databind.ObjectMapperFactory;
@@ -12,6 +28,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests for {@link OriginConfig}.
+ *
+ * @author Spencer Pearson (spencerpearson at dropbox dot com)
+ */
 public class OriginConfigTest {
     @Test
     public void testDeserialization() throws IOException {
