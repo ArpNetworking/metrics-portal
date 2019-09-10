5908e708be85044f8412090215ce31858d630f95
Spencer Pearson <spencerpearson@dropbox.com>
2019-09-10 13:17:57 -0700

    make DevToolsFactory pull OriginConfigs out of its Config

--- app/com/arpnetworking/metrics/portal/reports/impl/chrome/DefaultDevToolsFactory.java
+++ app/com/arpnetworking/metrics/portal/reports/impl/chrome/DefaultDevToolsFactory.java
@@ -29,6 +29,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
 
 import java.io.IOException;
 import java.time.Duration;
