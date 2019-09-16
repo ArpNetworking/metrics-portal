cc53f3001d234cef86ecdd78635d1e090d74253a
Spencer Pearson <spencerpearson@dropbox.com>
2019-09-16 15:09:38 -0700

    Revert "fix and strengthen report-serialization tests"
    This reverts commit 5a23edf852c422fadd27c93432003a5f444f344b.

--- app/models/internal/impl/DefaultReport.java
+++ app/models/internal/impl/DefaultReport.java
@@ -31,7 +31,6 @@ import models.internal.reports.ReportFormat;
 import models.internal.reports.ReportSource;
 import net.sf.oval.constraint.NotEmpty;
 import net.sf.oval.constraint.NotNull;
-import net.sf.oval.constraint.ValidateWithMethod;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -246,17 +245,11 @@ public final class DefaultReport implements Report {
         @NotNull
         private ReportSource _source;
         @NotNull
-        @ValidateWithMethod(methodName = "validateRecipientsPresent", parameterType = ImmutableSetMultimap.class)
         private ImmutableSetMultimap<ReportFormat, Recipient> _recipients;
         @NotNull
         private Schedule _schedule;
         @NotNull
         private Duration _timeout;
-
-        @SuppressFBWarnings(value = "UPM_UNCALLED_PRIVATE_METHOD", justification = "invoked reflectively by @ValidateWithMethod")
-        private boolean validateRecipientsPresent(final ImmutableSetMultimap<ReportFormat, Recipient> result) {
-            return !_recipients.isEmpty();
-        }
     }
 
 }
--- test/java/models/view/ReportSerializationTest.java
+++ test/java/models/view/ReportSerializationTest.java
@@ -16,17 +16,9 @@
 
 package models.view;
 
-import com.arpnetworking.commons.jackson.databind.ObjectMapperFactory;
-import com.arpnetworking.metrics.portal.TestBeanFactory;
 import com.arpnetworking.metrics.portal.reports.RecipientType;
 import com.arpnetworking.utility.test.ResourceHelper;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.Lists;
-import models.internal.impl.DefaultRecipient;
-import models.internal.impl.DefaultReport;
-import models.internal.reports.ReportFormat;
 import models.internal.scheduling.Period;
 import models.view.impl.HtmlReportFormat;
 import models.view.impl.OneOffSchedule;
@@ -35,7 +27,6 @@ import models.view.impl.WebPageReportSource;
 import models.view.reports.Recipient;
 import models.view.reports.Report;
 import models.view.scheduling.Schedule;
-import net.sf.oval.exception.ConstraintsViolatedException;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -43,7 +34,6 @@ import java.net.URI;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -54,74 +44,58 @@ import static org.junit.Assert.assertEquals;
  * @author Spencer Pearson (spencerpearson at dropbox dot com)
  */
 public final class ReportSerializationTest {
-
-    @Test
-    public void testDeserializeOfSerialize() throws IOException {
-        final models.internal.reports.Report originalInternal = TestBeanFactory.createReportBuilder().build();
-        final Report originalView = Report.fromInternal(originalInternal);
-        final String json = MAPPER.writeValueAsString(originalView);
-        final Report deserializedView = MAPPER.readValue(json, Report.class);
-        final models.internal.reports.Report deserializedInternal = deserializedView.toInternal();
-
-        assertEquals(originalInternal, deserializedInternal);
-    }
-
     @Test
     public void testValidReport() throws IOException {
-        final models.internal.reports.Report expected = new DefaultReport.Builder()
-                .setId(UUID.fromString("11111111-1111-1111-1111-111111111111"))
-                .setName("My Report Name")
-                .setTimeout(Duration.parse("PT1M"))
-                .setReportSource(new models.internal.impl.WebPageReportSource.Builder()
-                        .setId(UUID.fromString("22222222-2222-2222-2222-222222222222"))
-                        .setUri(URI.create("https://example.com"))
-                        .setTitle("My Report Title")
-                        .setIgnoreCertificateErrors(false)
-                        .setTriggeringEventName("myTriggeringEventName")
-                        .build())
-                .setSchedule(new com.arpnetworking.metrics.portal.scheduling.impl.OneOffSchedule.Builder()
-                        .setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"))
-                        .build())
-                .setRecipients(ImmutableSetMultimap.<ReportFormat, models.internal.reports.Recipient>builder()
-                        .put(
-                                new models.internal.impl.HtmlReportFormat.Builder().build(),
-                                new models.internal.impl.DefaultRecipient.Builder()
-                                        .setType(RecipientType.EMAIL)
-                                        .setId(UUID.fromString("33333333-3333-3333-3333-333333333333"))
-                                        .setAddress("nobody@example.com")
-                                .build()
-                        )
-                        .build())
-                .build();
+        final Report expected = new Report();
+        expected.setId(UUID.fromString("11111111-1111-1111-1111-111111111111"));
+        expected.setName("My Report Name");
+
+        final WebPageReportSource expectedSource = new WebPageReportSource();
+        expectedSource.setId(UUID.fromString("22222222-2222-2222-2222-222222222222"));
+        expectedSource.setUri(URI.create("https://example.com"));
+        expectedSource.setTitle("My Report Title");
+        expectedSource.setIgnoreCertificateErrors(false);
+        expectedSource.setTriggeringEventName("myTriggeringEventName");
+        expected.setSource(expectedSource);
+
+        final OneOffSchedule expectedSchedule = new OneOffSchedule();
+        expectedSchedule.setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"));
+        expected.setSchedule(expectedSchedule);
+
+        final Recipient expectedRecipient = new Recipient();
+        expectedRecipient.setType(RecipientType.EMAIL);
+        expectedRecipient.setId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
+        expectedRecipient.setAddress("nobody@example.com");
+        expectedRecipient.setFormat(new HtmlReportFormat());
+        expected.setRecipients(Lists.newArrayList(expectedRecipient));
+
         final Report actual = loadResourceAs("testValidReport", Report.class);
-        assertEquals(expected, actual.toInternal());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testValidPeriodicSchedule() throws IOException {
-        final com.arpnetworking.metrics.portal.scheduling.Schedule expected = new com.arpnetworking.metrics.portal.scheduling.impl.PeriodicSchedule.Builder()
-                .setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"))
-                .setRunUntil(Instant.parse("2020-01-01T00:00:00Z"))
-                .setPeriod(ChronoUnit.DAYS)
-                .setZone(ZoneId.of("America/Los_Angeles"))
-                .setOffset(Duration.parse("PT4H"))
-                .build();
+        final PeriodicSchedule expected = new PeriodicSchedule();
+        expected.setRunAtAndAfter(Instant.parse("2019-01-01T00:00:00Z"));
+        expected.setRunUntil(Instant.parse("2020-01-01T00:00:00Z"));
+        expected.setPeriod(Period.DAILY);
+        expected.setZone(ZoneId.of("America/Los_Angeles"));
+        expected.setOffset(Duration.parse("PT4H"));
 
         final Schedule actual = loadResourceAs("testValidPeriodicSchedule", Schedule.class);
-        assertEquals(expected, actual.toInternal());
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testValidRecipient() throws IOException {
-        final models.internal.reports.Recipient expected = new DefaultRecipient.Builder()
-                .setType(RecipientType.EMAIL)
-                .setId(UUID.fromString("33333333-3333-3333-3333-333333333333"))
-                .setAddress("nobody@example.com")
-                .build();
+        final Recipient expected = new Recipient();
+        expected.setType(RecipientType.EMAIL);
+        expected.setId(UUID.fromString("33333333-3333-3333-3333-333333333333"));
+        expected.setAddress("nobody@example.com");
+        expected.setFormat(new HtmlReportFormat());
 
         final Recipient actual = loadResourceAs("testValidRecipient", Recipient.class);
-        assertEquals(expected, actual.toInternal());
-        assertEquals(new HtmlReportFormat(), actual.getFormat());
+        assertEquals(expected, actual);
     }
 
     @Test(expected = com.fasterxml.jackson.databind.exc.InvalidFormatException.class)
@@ -129,16 +103,7 @@ public final class ReportSerializationTest {
         loadResourceAs("testInvalidRecipientNoSuchType", Recipient.class);
     }
 
-    @Test(expected = ConstraintsViolatedException.class)
-    public void testInvalidNoRecipients() throws IOException {
-        final Report report = loadResourceAs("testValidReport", Report.class);
-        report.setRecipients(ImmutableList.of());
-        report.toInternal();
-    }
-
     private <T> T loadResourceAs(final String resourceSuffix, final Class<T> clazz) throws IOException {
         return ResourceHelper.loadResourceAs(getClass(), resourceSuffix, clazz);
     }
-
-    private static final ObjectMapper MAPPER = ObjectMapperFactory.getInstance();
 }
