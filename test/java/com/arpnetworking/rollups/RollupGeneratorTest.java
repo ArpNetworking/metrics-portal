a92873712ba7a5f456171b54856eb79274ffcc34
Spencer Pearson <speeze.pearson+mportal@gmail.com>
2020-04-29 15:55:01 -0700

    improve test

--- test/java/com/arpnetworking/rollups/RollupGeneratorTest.java
+++ test/java/com/arpnetworking/rollups/RollupGeneratorTest.java
@@ -161,7 +161,7 @@ public class RollupGeneratorTest {
         final TagNamesMessage tagNamesMessage = _probe.expectMsgClass(TagNamesMessage.class);
         assertFalse(tagNamesMessage.isFailure());
         assertEquals("metric", tagNamesMessage.getMetricName());
-        assertEquals(2, tagNamesMessage.getTags().size());
+        assertEquals(ImmutableMultimap.of("tag1", "value1", "tag2", "value2"), tagNamesMessage.getTags());
 
         verify(_kairosDbClient, times(1)).queryMetricTags(captor.capture());
         final TagsQuery tagQuery = captor.getValue();
