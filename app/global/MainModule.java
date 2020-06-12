4aaf7b8fcd99b5894311b3cd36c456e186858c22
Christian Briones <cwbriones@gmail.com>
2020-06-11 18:11:24 -0700

    add runtime DI bindings for QueryExecutor

--- app/global/MainModule.java
+++ app/global/MainModule.java
@@ -232,10 +232,7 @@ public class MainModule extends AbstractModule {
     @Singleton
     @Provides
     @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD") // Invoked reflectively by Guice
-    private AlertExecutionContext provideAlertExecutionContext(
-            final Config config,
-            final QueryExecutor executor
-    ) {
+    private AlertExecutionContext provideAlertExecutionContext(final Config config) {
         final FiniteDuration interval = ConfigurationHelper.getFiniteDuration(config, "alerting.execution.defaultInterval");
         final Schedule defaultAlertSchedule = new PeriodicSchedule.Builder()
                 .setPeriod(TimeAdapters.toChronoUnit(interval.unit()))
@@ -243,7 +240,7 @@ public class MainModule extends AbstractModule {
                 .setZone(ZoneOffset.UTC)
                 .setRunAtAndAfter(Instant.MIN)
                 .build();
-        return new AlertExecutionContext(defaultAlertSchedule, executor);
+        return new AlertExecutionContext(defaultAlertSchedule);
     }
 
     @Singleton
