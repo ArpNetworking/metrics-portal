c1fa45b80c581e0fda8d52d2a8cd7719a892dbce
Christian Briones <cwbriones@gmail.com>
2020-07-29 12:16:13 -0700

    remove unused import

--- test/java/controllers/AlertControllerTest.java
+++ test/java/controllers/AlertControllerTest.java
@@ -46,7 +46,6 @@ import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Stream;
