28d660c07f1747f10aa35b0392228162344c249c
Ville Koskela <ville@koskelafamily.com>
2021-02-16 11:21:20 -0800

    Add client side aggregation of telemetry metrics for the same series.

--- app/assets/javascripts/classes/Series.ts
+++ app/assets/javascripts/classes/Series.ts
@@ -24,6 +24,19 @@ class Series {
     bars: any = { show: false, stacked: false };
     color: string = "black";
     colorSubscription: KnockoutSubscription;
+    merger: (x: number, y: number) => number;
+
+    static mergeBySum = function(x: number, y: number): number {
+        return x + y;
+    };
+
+    static mergeByMin = function(x: number, y: number): number {
+        return x < y ? x : y;
+    };
+
+    static mergeByMax = function(x: number, y: number): number {
+        return x > y ? x : y;
+    };
 
     static defaultPoints() {
         return  { show: true };
@@ -37,23 +50,43 @@ class Series {
         return { show: false, stacked: false };
     }
 
-    constructor(label: string, color: string) {
+    constructor(label: string, color: string, statistic: string) {
         this.color = color;
         this.label = label;
+        if (statistic == "sum" || statistic == "count") {
+            this.merger = Series.mergeBySum;
+        } else if (statistic == "max") {
+            this.merger = Series.mergeByMax;
+        } else if (statistic == "min") {
+            this.merger = Series.mergeByMin;
+        } else {
+            // Obviously this is very broken for percentiles.
+            this.merger = Series.mergeByMax;
+        }
     }
 
     pushData(timestamp: number, dataValue: number, paused: boolean) {
-        if (this.data.length == 0 || this.data[this.data.length - 1][0] < timestamp) {
-            if (paused) {
-                this.buffer.push([timestamp, dataValue]);
-            } else {
-                if (this.buffer.length > 0) {
-                    this.data = this.data.concat(this.buffer);
-                    this.buffer = [];
-                }
-
-                this.data.push([timestamp, dataValue]);
+        if (paused) {
+            // While paused any values that would merge into data are dropped
+            this.pushInternal(this.buffer, timestamp, dataValue);
+        } else {
+            if (this.buffer.length > 0) {
+                this.data = this.data.concat(this.buffer);
+                this.buffer = [];
             }
+            this.pushInternal(this.data, timestamp, dataValue);
+        }
+    }
+
+    pushInternal(target: number [][], timestamp: number, dataValue: number) {
+        // This only supports in-order merge (for now)
+        if (target.length == 0 || target[target.length - 1][0] < timestamp) {
+            target.push([timestamp, dataValue]);
+        } else if (target[target.length - 1][0] == timestamp) {
+            const currentValue = target[target.length - 1][1]
+            target[target.length - 1][1] = this.merger(currentValue, dataValue);
+        } else {
+            console.log("Received out of order data: " + timestamp + " vs" + target[target.length - 1][0] + " for " + this.label);
         }
     }
 }
