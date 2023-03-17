0f0841ecc510d7b4b9d2bd442355e7af02b53285
Brandon Arp <brandonarp@gmail.com>
2023-03-17 12:16:45 -0700

    Add Akka membership health provider (#532)
    * add a health provider that looks at akka membership
    * use akka membership health checker by default

---
+++ app/com/arpnetworking/metrics/portal/health/AkkaMembershipHealthProvider.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023 Brandon Arp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arpnetworking.metrics.portal.health;
+
+import akka.actor.ActorRef;
+import akka.pattern.Patterns;
+import com.arpnetworking.steno.Logger;
+import com.arpnetworking.steno.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Implementation of {@link HealthProvider} interface which looks at the Akka cluster
+ * state to determine health. If the host is a member and
+ *
+ * @author Brandon Arp (brandon dot arp at inscopemetrics dot io)
+ */
+public class AkkaMembershipHealthProvider implements HealthProvider {
+    /**
+     * Public constructor.
+     *
+     * @param statusActor the {@link StatusActor} to retrieve the cluster status from.
+     */
+    @Inject
+    public AkkaMembershipHealthProvider(@Named("status") final ActorRef statusActor) {
+        _statusActor = statusActor;
+    }
+
+    @Override
+    public boolean isHealthy() {
+        try {
+            return ask(_statusActor, new StatusActor.HealthRequest(), Boolean.FALSE).toCompletableFuture().get(3, TimeUnit.SECONDS);
+        } catch (final InterruptedException | TimeoutException | ExecutionException e) {
+            return false;
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> CompletionStage<T> ask(final ActorRef actor, final Object request, @Nullable final T defaultValue) {
+        return
+                Patterns.ask(
+                                actor,
+                                request,
+                                Duration.ofSeconds(3))
+                        .thenApply(o -> (T) o)
+                        .exceptionally(throwable -> {
+                            LOGGER.error()
+                                    .setMessage("error when routing ask")
+                                    .addData("actor", actor)
+                                    .addData("request", request)
+                                    .setThrowable(throwable)
+                                    .log();
+                            return defaultValue;
+                        });
+    }
+
+    private final ActorRef _statusActor;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AkkaMembershipHealthProvider.class);
+}
---
+++ test/java/com/arpnetworking/metrics/portal/health/AkkaMembershipHealthProviderTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 Brandon Arp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arpnetworking.metrics.portal.health;
+
+import akka.actor.ActorRef;
+import com.arpnetworking.commons.akka.BaseActorTest;
+import com.arpnetworking.utility.test.SimpleReplierActor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for the {@link StatusActor} actor.
+ *
+ * @author Brandon Arp (brandon dot arp at inscopemetrics dot io)
+ */
+public class AkkaMembershipHealthProviderTest extends BaseActorTest {
+
+    @Before
+    @Override
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void recognizesUnhealthy() {
+        final ActorRef unhealthyActor = getSystem().actorOf(SimpleReplierActor.props(false));
+        final AkkaMembershipHealthProvider provider = new AkkaMembershipHealthProvider(unhealthyActor);
+        final boolean isHealthy = provider.isHealthy();
+        assertFalse(isHealthy);
+    }
+
+    @Test
+    public void recognizesHealthy() {
+        final ActorRef healthyActor = getSystem().actorOf(SimpleReplierActor.props(true));
+        final AkkaMembershipHealthProvider provider = new AkkaMembershipHealthProvider(healthyActor);
+        final boolean isHealthy = provider.isHealthy();
+        assertTrue(isHealthy);
+    }
+}
+
