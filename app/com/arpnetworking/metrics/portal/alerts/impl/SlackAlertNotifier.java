62510d01ea5559918a69c732245e8dc5e3a518e0
Brandon Arp <brandonarp@gmail.com>
2023-05-12 14:47:21 -0700

    add wsclient

--- app/com/arpnetworking/metrics/portal/alerts/impl/SlackAlertNotifier.java
+++ app/com/arpnetworking/metrics/portal/alerts/impl/SlackAlertNotifier.java
@@ -1,20 +1,36 @@
+/*
+ * Copyright 2023 Inscope Metrics Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.arpnetworking.metrics.portal.alerts.impl;
 
-import com.arpnetworking.commons.builder.Builder;
 import com.arpnetworking.commons.builder.OvalBuilder;
 import com.arpnetworking.metrics.portal.alerts.AlertNotifier;
+import com.fasterxml.jackson.annotation.JacksonInject;
 import models.internal.alerts.Alert;
 import models.internal.alerts.AlertEvaluationResult;
 import net.sf.oval.constraint.NotEmpty;
 import net.sf.oval.constraint.NotNull;
+import play.libs.ws.WSClient;
 
 import java.util.concurrent.CompletionStage;
-import java.util.function.Function;
 
 public class SlackAlertNotifier implements AlertNotifier {
     private SlackAlertNotifier(final Builder builder) {
         _apiKey = builder._apiKey;
         _messagePostUrl = builder._messagePostUrl;
+        _wsClient = builder._wsClient;
     }
 
     @Override
@@ -24,6 +40,7 @@ public class SlackAlertNotifier implements AlertNotifier {
 
     private final String _apiKey;
     private final String _messagePostUrl;
+    private final WSClient _wsClient;
 
     public static class Builder extends OvalBuilder<SlackAlertNotifier> {
         /**
@@ -63,5 +80,8 @@ public class SlackAlertNotifier implements AlertNotifier {
         @NotNull
         @NotEmpty
         private String _messagePostUrl;
+        @NotNull
+        @JacksonInject
+        private WSClient _wsClient;
     }
 }
