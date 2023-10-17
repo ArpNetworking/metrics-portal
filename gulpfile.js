1affaba83b90b51fef032054a5c3e96d5dbdab84
Brandon Arp <brandonarp@gmail.com>
2023-10-17 01:50:49 -0700

    update gulp, add npm copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
+import gulp from 'gulp';
+import tscript from 'gulp-typescript';
+import {deleteAsync as del} from 'del';
+import npmDist from 'gulp-npm-dist';
 
-var tsProject = ts.createProject('tsconfig.json');
+const tsProject = tscript.createProject('tsconfig.json');
 
-gulp.task('default', function() {
+export function ts() {
     console.log("We're starting...")
     console.log("tsProject.config = ", tsProject.config);
     var tsResult = tsProject.src()
         .pipe(tsProject());
 
     return tsResult.js.pipe(gulp.dest('target/sbt/web/public/main/javascripts'));
-});
+}
+
+export function copy() {
+    return gulp.src(npmDist(), {base:'./node_modules'})
+        .pipe(gulp.dest('target/sbt/web/public/main/lib'));
+}
+
+export function clean() {
+    // You can use multiple globbing patterns as you would with `gulp.src`,
+    // for example if you are using del 2.0 or above, return its promise
+    return del([ 'target/sbt/web/public/main' ]);
+}
+
+const build = gulp.series(clean, gulp.parallel(ts, copy));
+
+export default build;
