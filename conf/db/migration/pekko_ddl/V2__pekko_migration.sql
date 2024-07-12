34c9512417177ad8e59c180e1190156e8ddc93c9
Brandon Arp <brandonarp@gmail.com>
2024-07-12 16:35:16 -0700

    new schema and cleanup

--- conf/db/migration/pekko_ddl/V2__pekko_migration.sql
+++ conf/db/migration/pekko_ddl/V2__pekko_migration.sql
@@ -14,4 +14,78 @@
  * limitations under the License.
  */
 
-ALTER SCHEMA akka RENAME TO pekko;
+
+DROP TABLE IF EXISTS akka.event_journal;
+DROP TABLE IF EXISTS akka.event_tag;
+DROP TABLE IF EXISTS akka.event_snapshot;
+DROP TABLE IF EXISTS akka.durable_state;
+
+DROP SCHEMA IF EXISTS akka CASCADE;
+
+DROP TABLE IF EXISTS pekko.event_journal;
+DROP TABLE IF EXISTS pekko.event_tag;
+DROP TABLE IF EXISTS pekko.event_snapshot;
+DROP TABLE IF EXISTS pekko.durable_state;
+
+CREATE TABLE IF NOT EXISTS pekko.event_journal(
+    ordering BIGSERIAL,
+    persistence_id VARCHAR(255) NOT NULL,
+    sequence_number BIGINT NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE NOT NULL,
+
+    writer VARCHAR(255) NOT NULL,
+    write_timestamp BIGINT,
+    adapter_manifest VARCHAR(255),
+
+    event_ser_id INTEGER NOT NULL,
+    event_ser_manifest VARCHAR(255) NOT NULL,
+    event_payload BYTEA NOT NULL,
+
+    meta_ser_id INTEGER,
+    meta_ser_manifest VARCHAR(255),
+    meta_payload BYTEA,
+
+    PRIMARY KEY(persistence_id, sequence_number)
+    );
+
+CREATE UNIQUE INDEX event_journal_ordering_idx ON pekko.event_journal(ordering);
+
+CREATE TABLE IF NOT EXISTS pekko.event_tag(
+    event_id BIGINT,
+    tag VARCHAR(256),
+    PRIMARY KEY(event_id, tag),
+    CONSTRAINT fk_event_journal
+    FOREIGN KEY(event_id)
+    REFERENCES event_journal(ordering)
+    ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS pekko.snapshot (
+    persistence_id VARCHAR(255) NOT NULL,
+    sequence_number BIGINT NOT NULL,
+    created BIGINT NOT NULL,
+
+    snapshot_ser_id INTEGER NOT NULL,
+    snapshot_ser_manifest VARCHAR(255) NOT NULL,
+    snapshot_payload BYTEA NOT NULL,
+
+    meta_ser_id INTEGER,
+    meta_ser_manifest VARCHAR(255),
+    meta_payload BYTEA,
+
+    PRIMARY KEY(persistence_id, sequence_number)
+    );
+
+CREATE TABLE IF NOT EXISTS pekko.durable_state (
+    global_offset BIGSERIAL,
+    persistence_id VARCHAR(255) NOT NULL,
+    revision BIGINT NOT NULL,
+    state_payload BYTEA NOT NULL,
+    state_serial_id INTEGER NOT NULL,
+    state_serial_manifest VARCHAR(255),
+    tag VARCHAR,
+    state_timestamp BIGINT NOT NULL,
+    PRIMARY KEY(persistence_id)
+    );
+CREATE INDEX state_tag_idx on pekko.durable_state (tag);
+CREATE INDEX state_global_offset_idx on pekko.durable_state (global_offset);
